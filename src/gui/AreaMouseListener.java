ff655061fcbb88170c8630cf4b9dd80d6cd14056
Guilherme Dantas <guidanoli@hotmail.com>
2019-05-30 22:56:44 -0300

    AreaMouseListener documentation

--- src/gui/AreaMouseListener.java
+++ src/gui/AreaMouseListener.java
@@ -1,5 +1,19 @@
 package gui;
 
+/**
+ * 
+ * <p>The <code>AreaMouseListener</code> interface is suited for
+ * mouse events occurring inside a determined area on screen. The
+ * methods to be implemented follow:
+ * 
+ * <ul>
+ * <li>The {@link AreaMouseListener#action() action()} function
+ * is triggered whenever the user left clicks on the area.</li>
+ * </ul>
+ * 
+ * @author guidanoli
+ *
+ */
 public interface AreaMouseListener {
 	public void action();
 }
